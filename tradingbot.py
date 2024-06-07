ac65ef917b9a88b6e9028c197a1cf1e4d6dfbb25
Kartikay <sharma.kartikay152001@gmail.com>
2024-06-08 00:39:03 +0530

    Update

--- tradingbot.py
+++ tradingbot.py
@@ -5,9 +5,14 @@ from lumibot.traders import Trader
 from datetime import datetime
 from dotenv import load_dotenv, dotenv_values
 from alpaca_trade_api import REST
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce, QueryOrderStatus
+from alpaca.trading.requests import GetOrdersRequest
 from timedelta import Timedelta
 from finbert_utils import estimate_sentiment
 from NLP_Strategy import MLTrader
+
 
 config=dotenv_values(".env")
 API_KEY = config['API_KEY']
@@ -19,6 +24,21 @@ ALPACA_CREDS = {
     "API_SECRET":API_SECRET,
     "PAPEr":True
 }   
+
+# Function to create and place order
+def place_order(trading_client, symbol, qty, side):
+  market_order_data = MarketOrderRequest(
+        symbol=symbol,
+        qty=qty,
+        side=side,
+        time_in_force=TimeInForce.DAY
+        )
+
+# Preparing Market order
+  market_order = trading_client.submit_order(
+        order_data=market_order_data
+        )
+  
 
 start_date = datetime(2023,1,1)
 end_date = datetime(2024,5,10)
